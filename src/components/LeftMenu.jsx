5d3388dc838de615ff8146faa144921aacff1f7f
Ivan Weber <b24b24testing.ru>
2020-04-10 12:06:43 +0300

    adaptive with left menu but issue with width reduction :(

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import s from './LeftMenu.module.css';
 
 
 const LeftMenu = (props) => {
     return (
 
-        <div className="left-column menu">
+        <div className={s['left-column']}>
             <ul>
                 <li><a>Главная страница</a></li>
                 <li><a>Новости</a></li>
                 <li><a>Группы</a></li>
-                <li><a>Настройки</a></li>
+                <li><a>Настройки</a></li> 
                 <li><a>Выход</a></li>
             </ul>
         </div>
