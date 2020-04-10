f3b3d75b49c8f0945fb0a70fa672ffff802f5c3f
Ivan Weber <b24b24testing.ru>
2020-04-10 18:22:12 +0300

    Header component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './Header.module.css';
+
+
+const Header = (props) => {
+    return (
+        <header className={s['header-hwp']}>
+            <h1>Hello World Project</h1>
+        </header>
+    )
+};
+
+export default Header;
+
