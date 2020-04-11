77ebbac68712a4361673c8a3781ab7b57a4b66ee
Ivan Weber <b24b24testing.ru>
2020-04-11 03:49:54 +0300

    react router dom installed

--- src/components/Content/MainContentWrapper/MainContentWrapper.jsx
+++ src/components/Content/MainContentWrapper/MainContentWrapper.jsx
@@ -8,8 +8,8 @@ import NewsItem from './NewsItem/NewsItem';
 const MainContentWrapper = (props) => {
     return (
         <div className={s['main-content-wrapper']}>
-            <NewsItem />
-            <NewsItem />
+            <NewsItem head='div-1' par='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
+            <NewsItem head='div-1' par='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
         </div>
     )
 };
