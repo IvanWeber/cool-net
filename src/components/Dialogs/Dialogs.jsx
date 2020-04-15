ddf520a3277dd33b66d36e5c8a82406c8309371e
Ivan Weber <b24b24testing.ru>
2020-04-15 08:30:59 +0300

    first structure of dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import s from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+    <div className={s.dialog + ' ' + s.active}>
+    <NavLink to={path}>{props.name}</NavLink>
+    </div>
+    )
+}
+
+const Message = (props => {
+    return (
+        <div className={s.dialog}>{props.message}</div>
+    )
+})
 
 const Dialogs = (props) => {
     return (
-        <div>
-            <p>Dialogs-component</p>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem id={1} name="Андрей" />
+                <DialogItem id={2} name="Иван" />
+                <DialogItem id={3} name="Анна" />
+                <DialogItem id={4} name="Сергей" />
+            </div>
+
+            <div className={s.messages}>
+                <Message message="Hi" />
+                <Message message="How are you?" />
+                <Message message="Yo yo" />
+                <Message message="I'm okay" />
+            </div>
         </div>
     )
 };
 
 export default Dialogs;
+
 
