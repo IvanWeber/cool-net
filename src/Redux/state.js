24fe196f4c58b34fada6d7edeea61f88c451a589
Ivan Weber <ivanbdn@yandex.ru>
2020-05-01 11:58:40 +0300

    state packed in store, oop and bind topic learned, 36 37 lections

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,57 +1,65 @@
-let rerenderEntireTree = () => {
-    console.log('State changed');
-};
+export let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 1, message: 'It\'s my first post', likesCount: 11},
+                {id: 1, message: 'Blabla', likesCount: 11},
+                {id: 1, message: 'Dada', likesCount: 11},
+            ],
+            newPostText: 'it-kamasutra.comaaa'
+        },  
+    
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Андрей'}, 
+                { id: 2, name: 'Иван'}, 
+                { id: 3, name: 'Анна'}, 
+                { id: 4, name: 'Сергей'}
+              ],
+            messages: [
+                { id: 1, message: 'Hi'}, 
+                { id: 2, message: 'How are you?'}, 
+                { id: 3, message: 'Yo yo'}, 
+                { id: 4, message: "I'm okay"}
+              ]
+        },
+        
+        sidebar: {}
+    },
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 1, message: 'It\'s my first post', likesCount: 11},
-            {id: 1, message: 'Blabla', likesCount: 11},
-            {id: 1, message: 'Dada', likesCount: 11},
-        ],
-        newPostText: 'it-kamasutra.comaaa'
-    },  
+    getState () {
+        return this._state;
+    },
 
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Андрей'}, 
-            { id: 2, name: 'Иван'}, 
-            { id: 3, name: 'Анна'}, 
-            { id: 4, name: 'Сергей'}
-          ],
-        messages: [
-            { id: 1, message: 'Hi'}, 
-            { id: 2, message: 'How are you?'}, 
-            { id: 3, message: 'Yo yo'}, 
-            { id: 4, message: "I'm okay"}
-          ]
+    _callSubscriber () {
+        console.log('State changed');
+    }, 
+
+    addPost () {
+        let newPost = {
+            id: this._state.profilePage.posts.length + 1, 
+            message: this._state.profilePage.newPostText, 
+            likesCount: Math.round(Math.random()*100)
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+        console.log(this._state.profilePage.newPostText);
+        console.log(this._state.profilePage);
     },
     
-    sidebar: {}
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: state.profilePage.posts.length + 1, 
-        message: state.profilePage.newPostText, 
-        likesCount: Math.round(Math.random()*100)
+    subscribe (observer) {
+        this._callSubscriber = observer;
     }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-};
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-    console.log(state.profilePage.newPostText);
-    console.log(state.profilePage);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
 };
 
 
-export default state;
+export default store;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import state from './Redux/state.js';
+// import state from './Redux/state.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {addPost, updateNewPostText, subscribe} from './Redux/state.js';
+import {store} from './Redux/state.js';
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
 
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
