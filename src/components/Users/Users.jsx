04c6702e0b3c98e979f62a2eb130eab06ce662a7
Ivan Weber <ivanbdn@yandex.ru>
2020-05-25 22:30:00 +0300

    ajax relocated in UsersContainer, lection 56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,74 +1,36 @@
 import React from 'react';
-import styles from './users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/users.png'
+import styles from "./users.module.css";
+import userPhoto from "../../assets/images/users.png";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    constructor (props) {
-        super(props);
-        alert('NEW');
-    }
-
-    componentDidMount () {
-        alert('did mount');
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
-    }
-
-    // getUsers = () => {
-
-    //     if (this.props.users.length === 0) {
-    //         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-    //             this.props.setUsers(response.data.items);
-    //         });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     
-    //     //     props.setUsers([
-    //     //     {id: 1, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'} },
-    //     //     {id: 2, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'} },
-    //     //     {id: 3, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: false, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'} },
-    //     // ]);
-    //     }
-    // }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        alert('did mount');
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
+    let pages = [];
+    
+    for (let i=1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render() {
+    console.log(props.totalUsersCount);
 
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-
-        for (let i=1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return <div>
+    return <div>
             <div>
                 {pages.map( p => {
-                    return <span className={this.props.currentPage === p && styles.selectedPage} onClick={(e) => {this.onPageChanged(p)}}>{p} </span>
+                    return <span className={props.currentPage === p && styles.selectedPage} onClick={(e) => {props.onPageChanged(p)}}>{p} </span>
                 })}
             </div>
                 {/* <button onClick={this.getUsers}>Get Users</button> */}
         {
-            this.props.users.map(u => <div key={u.id}>
+            props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small !== null ? u.photos.small: userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed 
-                            ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> 
-                            : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
+                            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
+                            : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
                     </div>
                 </span>
                 <span>
@@ -84,7 +46,6 @@ class Users extends React.Component {
             </div>)
         }
     </div>
-    }
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
-import Users from './Users';
 import {connect} from 'react-redux';
 import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersTotalCountAC} from '../../Redux/users-reducer';
+import Users from './Users';
+import * as axios from 'axios';
+
+class UsersContainer extends React.Component {
+
+    constructor (props) {
+        super(props);
+        alert('NEW');
+    }
+
+    componentDidMount () {
+        alert('did mount');
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+                console.log(response.data.totalCount);
+            });
+    }
+
+    // getUsers = () => {
+
+    //     if (this.props.users.length === 0) {
+    //         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    //             this.props.setUsers(response.data.items);
+    //         });
+    
+    //     //     props.setUsers([
+    //     //     {id: 1, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: false, fullName: 'Dmitry', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'} },
+    //     //     {id: 2, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: true, fullName: 'Sasha', status: 'I am a boss too', location: {city: 'Moscow', country: 'Russia'} },
+    //     //     {id: 3, photoUrl: 'https://cdn.pixabay.com/photo/2014/09/05/18/32/old-books-436498_960_720.jpg', followed: false, fullName: 'Andrew', status: 'I am a boss too', location: {city: 'Kiev', country: 'Ukraine'} },
+    //     // ]);
+    //     }
+    // }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        alert('did mount');
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+                // this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    render() {
+
+        return <Users   totalUsersCount={this.props.totalUsersCount}
+                        pageSize={this.props.pageSize}
+                        currentPage={this.props.currentPage}
+                        onPageChanged={this.onPageChanged}
+                        users={this.props.users}
+                        follow={this.props.follow}
+                        unfollow={this.props.unfollow}
+        /> 
+    }
+}
 
 let mapStateToProps = (state) => { //прокидываем через пропсы даннные
+    console.log(state.usersPage.totalUsersCount);
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
@@ -32,4 +87,4 @@ let mapDispatchToProps = (dispatch) => {// прокидываем через п�
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
